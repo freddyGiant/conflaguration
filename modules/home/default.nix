0e0ad5be0122381c148d17546da5f4592ab5e649
vrad <vrad+notarealemail@vradford.com>
2026-04-14 11:23:04 -0400

    Add various graphical apps

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -13,15 +13,36 @@
 
   home.username = lib.mkDefault "vrad";
 
+  # NOTE: some of these don't really make sense on priveleged/remote machines.
+  # worst case, they're just useless.
   home.packages = with pkgs; [
-    ncdu
+    # cli, or small utility
+    brightnessctl
     killall
+    ncdu
+    pavucontrol
+    # rnote
+    shotman
+    wev
+    wl-clipboard
+
+    # heavier, graphical
+    kdePackages.dolphin
+    localsend
+    prismlauncher
+    qalculate-qt
+    # also remove electron from flake.nix > permittedInsecurePackages
+    webcord
+    vlc
     styluslabs-write
+    zoom-us
   ];
 
   programs.fd.enable = true;
   programs.fzf.enable = true;
   programs.ripgrep.enable = true;
+  programs.tofi  .enable = true;
+  programs.waybar.enable = true;
 
   # let home-manager enable/restart systemd services as appropriate
   systemd.user.startServices = "sd-switch";
