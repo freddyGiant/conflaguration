7ac03d2da80766e4b7428caeaf6eeac7d04a4ef5
vrad <vrad+notarealemail@vradford.com>
2026-04-26 17:45:44 -0400

    Fix fonts

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -1,21 +1,24 @@
-{
+{ pkgs, ... }: {
   # $ fc-list | grep -i 'font-name' | sed 's/^.*: //'
+
+  home.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    open-sans
+    # FIXME: where does this appear?
+    openmoji-color
+  ];
+
   fonts.fontconfig = {
     enable = true;
     antialiasing = true;
     # hinting = "full";
     subpixelRendering = "rgb";
+  };
 
-    packages = with pkgs; [
-      nerd-fonts.jetbrains-mono
-      open-sans
-      # FIXME: where does this appear?
-      openmoji-color
-    ];
-
-    defaultFonts = {
-      monospace = [ "JetBrainsMono NFM" ];
-      sans-serif = [ "open-sans" ];
-    };
+  fonts.fontconfig.defaultFonts = {
+    # FIXME: what is this actually called? see command at top lol
+    emoji = [ "openmoji-color" ];
+    monospace = [ "JetBrainsMono NFM" ];
+    sansSerif = [ "open-sans" ];
   };
 }
