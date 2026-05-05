c79942bcb68db1a93699cdaf255a046b50dd1683
vrad <vrad+notarealemail@vradford.com>
2026-05-04 22:01:39 -0400

    Ditch niri flake nixos module entirely (use nixpkgs)

--- modules/nixos/niri.nix
+++ modules/nixos/niri.nix
@@ -1,14 +1,24 @@
-{ config, inputs, pkgs, ... }: {
-  imports = [ inputs.niri.nixosModules.niri ];
+{ config, /* inputs, */ pkgs, ... }: {
+  # imports = [ inputs.niri.nixosModules.niri ];
   # uncomment if we want to switch to sodiboo's more up-to-date binary cache
   # nixpkgs.overlays = [ inputs.niri.overlays.niri ];
 
   programs.niri.enable = true;
-  programs.niri.package = pkgs.niri;
-  niri-flake.cache.enable = false;
+  # programs.niri.package = pkgs.niri;
+  # niri-flake.cache.enable = false;
 
   programs.uwsm.enable = true;
   programs.uwsm.waylandCompositors.niri.binPath = "${pkgs.niri}/bin/niri-session";
 
-  xdg.portal.wlr.enable = true;
+  # xdg.portal = {
+  #   enable = true;
+  #   xdgOpenUsePortal = true;
+  #   config.common.default = "*";
+  #   extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  # };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
 }
