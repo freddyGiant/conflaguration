6925ba1990b836f6c1784c7271ad0ffb22bab85a
vrad <vrad+notarealemail@vradford.com>
2026-05-04 22:02:47 -0400

    niri: add bash profile tty auto launch

--- modules/home/niri.nix
+++ modules/home/niri.nix
@@ -1,6 +1,7 @@
 # https://github.com/sodiboo/niri-flake/blob/main/docs.md
 # https://github.com/sodiboo/niri-flake?tab=readme-ov-file
-{ inputs, pkgs, ... }: {
+# top ten least favorite nix syntax choices
+{ inputs, lib, osConfig ? {}, pkgs, ... }: {
   imports = [ inputs.niri.homeModules.niri ];
 
   programs.niri.settings.binds = {
@@ -8,10 +9,34 @@
     "XF86AudioLowerVolume".action.spawn = ["wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-"];
   };
 
+  programs.bash.enable = true; # lol
+  # if we started supporting other display managers at the same time (not sure why we would do this) we could make an option for a general uwsm script(??)
+  programs.bash.profileExtra = lib.mkAfter (if
+    (osConfig.programs.uwsm.waylandCompositors or {}) ? niri
+  then
+    /* bash */ ''
+      if uwsm check may-start; then
+        exec uwsm start niri-session-uwsm.desktop
+      fi
+    ''
+  else
+    # is this really the behavior we want?
+    /* bash */ ''
+      if command -v uwsm; then
+        if uwsm check may-start && uwsm select; then
+          exec uwsm start default
+        fi
+      else
+        exec niri-session
+      fi
+    '');
+
   systemd.user.sessionVariables.NIXOS_OZONE_WL = "1";
 
   xdg.portal = {
     enable = true;
+    xdgOpenUsePortal = true;
+    config.common.default = "*";
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
